404a3cb2367e7436d01e22c8f9118a9651fb7686
StackSpot Admin <admin@stackspot.com>
2022-03-18 15:15:22 +0000

    Update public repository

--- templates/app/src/main/kotlin/group_id_folder/consumer/SqsConsumer.kt
+++ templates/app/src/main/kotlin/group_id_folder/consumer/SqsConsumer.kt
@@ -1,5 +1,6 @@
 package {{project_group_id}}.consumer
 
+import com.stackspot.logging.LoggerFactory
 import io.awspring.cloud.messaging.listener.SqsMessageDeletionPolicy
 import io.awspring.cloud.messaging.listener.annotation.SqsListener
 import org.springframework.stereotype.Service
@@ -7,8 +8,10 @@ import org.springframework.stereotype.Service
 @Service
 class SqsConsumer {
 
+    private val logger = LoggerFactory.resource(SqsConsumer::class)
+
     @SqsListener(value = ["\${sqs.queue.consume}"], deletionPolicy = SqsMessageDeletionPolicy.{{inputs.sqs_message_deletion_policy}})
     fun consume(data: String) {
-        println("Received: $data")
+        logger.info{ "Received: $data" }
     }
 }
